686c3e1de7960e2ce58e5ba047b6145bfad442d0
BadTheSolguy <67464944+BadTheSolguy@users.noreply.github.com>
2023-02-16 09:56:59 +0100

    Delete Database/RiservaBella directory

--- Database/RiservaBella/ADMINS.sql
+++
@@ -1,5 +0,0 @@
-INSERT into ADMINS (ID, USERNAME, PASS) values (1, "admin", "admin");
-INSERT into ADMINS (ID, USERNAME, PASS) values (2, "Ema", "ema1");
-INSERT into ADMINS (ID, USERNAME, PASS) values (3, "peppe", "peppe");
-INSERT into ADMINS (ID, USERNAME, PASS) values (4, "ste", "taninolove");
-INSERT into ADMINS (ID, USERNAME, PASS) values (5, "paky", "ruby");
